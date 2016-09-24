20b08bce877a056356386bccbffcc07ef4389319
rsimona <rsimona@gmail.com>
2016-09-24 23:18:13 +0200

    Afegit filtrar per any i paginació

--- src/main/java/org/koushik/javabrains/messenger/resources/MessageResource.java
+++ src/main/java/org/koushik/javabrains/messenger/resources/MessageResource.java
@@ -10,6 +10,7 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import org.koushik.javabrains.messenger.model.Message;
@@ -20,36 +21,43 @@ import org.koushik.javabrains.messenger.service.MessageService;
 @Produces(MediaType.APPLICATION_JSON)
 public class MessageResource {
 
-	MessageService messageService = new MessageService();
-	
-	@GET
-	public List<Message> getMessages() {
-		return messageService.getAllMessages();
-	}
+    MessageService messageService = new MessageService();
 
-	@POST
-	public Message addMessage(Message message) {
-		return messageService.addMessage(message);
-	}
-	
-	@PUT
-	@Path("/{messageId}")
-	public Message updateMessage(@PathParam("messageId") long id, Message message) {
-		message.setId(id);
-		return messageService.updateMessage(message);
-	}
-	
-	@DELETE
-	@Path("/{messageId}")
-	public void deleteMessage(@PathParam("messageId") long id) {
-		messageService.removeMessage(id);
-	}
-	
-	
-	@GET
-	@Path("/{messageId}")
-	public Message getMessage(@PathParam("messageId") long id) {
-		return messageService.getMessage(id);
-	}
-	
+    @GET
+    public List<Message> getMessages(@QueryParam("year") int year,
+            @QueryParam("start") int start,
+            @QueryParam("size") int size) {
+        if (year > 0) {
+            return messageService.getAllMessagesForYear(year);
+        }
+        if (start >= 0 && size > 0) {
+            return messageService.getAllMessagesPaginated(start, size);
+        }
+        return messageService.getAllMessages();
+    }
+
+    @POST
+    public Message addMessage(Message message) {
+        return messageService.addMessage(message);
+    }
+
+    @PUT
+    @Path("/{messageId}")
+    public Message updateMessage(@PathParam("messageId") long id, Message message) {
+        message.setId(id);
+        return messageService.updateMessage(message);
+    }
+
+    @DELETE
+    @Path("/{messageId}")
+    public void deleteMessage(@PathParam("messageId") long id) {
+        messageService.removeMessage(id);
+    }
+
+    @GET
+    @Path("/{messageId}")
+    public Message getMessage(@PathParam("messageId") long id) {
+        return messageService.getMessage(id);
+    }
+
 }
