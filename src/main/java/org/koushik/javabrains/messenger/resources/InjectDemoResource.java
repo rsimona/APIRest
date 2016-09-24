02ca44898939edab1f235cf500b4d85c6cff5e4d
rsimona <rsimona@gmail.com>
2016-09-25 00:05:03 +0200

    Afegits nous tipus de paràmetres

---
+++ src/main/java/org/koushik/javabrains/messenger/resources/InjectDemoResource.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.koushik.javabrains.messenger.resources;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.CookieParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.MatrixParam;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ *
+ * @author Ramon
+ */
+@Path("/injectdemo")
+@Consumes(MediaType.TEXT_PLAIN)
+@Produces(MediaType.TEXT_PLAIN)
+public class InjectDemoResource {
+
+    @GET
+    @Path("annotations")
+    public String getParamsUsingAnnotations(@MatrixParam("param") String matrixParam,
+            @HeaderParam("customHeaderValue") String headerParam,
+            @CookieParam("name") String cookie) {
+        return "Matrix param: " + matrixParam + " Header Param: " + headerParam + " Cookie Param: " + cookie;
+    }
+
+}
