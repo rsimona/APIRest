e8318133b4219a21e7a14e9e43dab35d848dd1c5
rsimona <rsimona@gmail.com>
2016-09-25 08:59:03 +0200

    Afegit exemple de l'anotació @BeanParam

--- src/main/java/org/koushik/javabrains/messenger/resources/InjectDemoResource.java
+++ src/main/java/org/koushik/javabrains/messenger/resources/InjectDemoResource.java
@@ -13,6 +13,10 @@ import javax.ws.rs.MatrixParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.UriInfo;
+
 
 /**
  *
---
+++ src/main/java/org/koushik/javabrains/messenger/resources/beans/MessageFilterBean.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.koushik.javabrains.messenger.resources.beans;
+
+import javax.ws.rs.QueryParam;
+
+/**
+ *
+ * @author Ramon
+ */
+public class MessageFilterBean {
+
+    private @QueryParam("year")
+    int year;
+    private @QueryParam("start")
+    int start;
+    private @QueryParam("size")
+    int size;
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public void setStart(int start) {
+        this.start = start;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+    
+    
+}
